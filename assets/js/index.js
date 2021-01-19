50aae9501ce2def269699fefd1155655377747a9
topgg <sumxmut@gmail.com>
2021-01-19 18:51:49 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,21 @@
 window.onload = function(){
 	console.log("感谢您的浏览，期待能够加入贵公司！我的 GitHub：https://github.com/topgg");
 }
+function password() {
+        var password=""
+        password=prompt('请输入密码以查看简历:','');
+        function custom_close()
+        {
+            window.opener=null;
+            window.open('','_self');
+            window.close();        
+        }   
+        if  (password !='xugaoyang')
+            {
+            
+            custom_close();            
+            //加这一段是为了避免有些浏览器不支持关闭浏览器的js ,所以请求跳转到一个新页面！
+            window.location.href='about:blank';
+            }
+        }
+document.write(password());
